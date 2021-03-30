94200b5316f4a3e73acca59fb358d4619beaf385
jennysol <jennyssoliver@gmail.com>
2021-03-30 07:41:52 -0300

    Middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
 app.listen(3000, () => {
     console.log( 'Backend follow up successfully! ')
