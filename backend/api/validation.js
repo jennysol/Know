19ac7b1fd77bca34061f1d72bade5d682a76810b
jennysol <jennyssoliver@gmail.com>
2021-04-03 00:36:34 -0300

    Add Validation

---
+++ backend/api/validation.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+    function existsOrErros(value, msg) {
+        if(!value) throw msg
+        if(Array.isArray(value) && value.length === 0) throw msg
+        if(typeof value === 'string' && !value.trim()) throw msg
+    }
+    
+    function notExistsOrError(value, msg) {
+        try {
+            existsOrErros(value, msg)
+        } catch(msg) {
+            return
+        }
+        throw msg
+    }
+    
+    function equalsOrError(valueA, valueB, msg) {
+        if(valueA !== valueB) throw msg
+    }
+
+    return { existsOrErros, notExistsOrError, equalsOrError }
+}
