935ae6e538fd3545dafcda5726f708bef3b355e3
jennysol <jennyssoliver@gmail.com>
2021-04-03 17:53:17 -0300

    Add toTree in categories

--- backend/api/category.js
+++ backend/api/category.js
@@ -89,5 +89,22 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const toTree = (categories, tree) => {
+        if(!tree) tree = categories.filter( c => !c.parentId)
+        tree = tree.map(parentNode => {
+            const isChild = node => node.parentId == parentNode.id
+            parentNode.children = toTree(categories, categories.filter(isChild))
+            return parentNode
+        })
+
+        return tree
+    }
+
+    const getTree = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(toTree(categories)))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById, getTree }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -11,10 +11,13 @@ module.exports = app => {
         .get(app.api.category.get)
         .post(app.api.category.save)
 
+    // Beware of an order that comes before categories/:id
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
-
-
+    
 } 
