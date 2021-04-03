7919b795f947f843057adf37dfd243c4522b11cb
jennysol <jennyssoliver@gmail.com>
2021-04-03 17:24:04 -0300

    Add API to Categories

---
+++ backend/api/category.js
@@ -0,0 +1,93 @@
+module.exports = app => {
+    const { existsOrError, notExistsOrError } = app.api.validation
+
+    const save = (req, res) => {
+        const category = { ...req.body }
+        if(req.params.id) category.id = req.params.id
+
+        try {
+            existsOrError(category.name, 'Nome não informado')
+        } catch(msg) {
+            return res.status(400).send(msg)
+        }
+
+        if(category.id) {
+            app.db('categories')
+                .update(category)
+                .where({ id: category.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('categories')
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = async (req, res) => {
+        try {
+            existsOrError(req.params.id, 'Código da Categoria não informado.')
+
+            const subcategory = await app.db('categories')
+                .where({ parentId: req.params.id })
+
+            notExistsOrError(subcategory, 'Categoria possui subcategorias.')
+
+            const articles = await app.db('articles')
+                .where({ categoryId: req.params.id })
+            notExistsOrError(articles, 'Categopria possui artigos.')
+
+            const rowsDeleted = await app.db('categories')
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId)
+            return parent.length ? parent[0] : null
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
+
+            while(parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
+
+            return { ...category, path }
+        })
+
+        categoriesWithPath.sort((a, b) => {
+            if(a.path < b.path) return -1
+            if(a.path > b.path) return 1
+            return 0
+        })
+
+        return categoriesWithPath
+    }
+
+    const get = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('categories')
+            .where({ id: req.params.id })
+            .first()
+            .then(category => res.json(category))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById }
+}
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,4 +6,15 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+
+
 } 
