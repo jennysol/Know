8b47ba089cea616a986828bb514958c6d16e9629
jennysol <jennyssoliver@gmail.com>
2021-04-08 21:51:36 -0300

    Add Categories with articles

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,3 +1,5 @@
+const queries = require('./queries')
+
 module.exports = app => {
     const { existsOrError } = app.api.validation
 
@@ -66,5 +68,22 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 1
+        const categories  = await app.db.raw(queries.categoryWithChildren, categoryId )
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({a: 'articles', u: 'users'})
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+            .limit(limit).offset(page * limit - limit )
+            .whereRaw('?? = ??', ['u.id', 'a.userId'])
+            .whereIn('categoryId', ids)
+            .orderBy('a.id', 'desc')
+            .then(articles => res.json(articles))
+            .cath(err => res.status(500).send(err))
+
+    }
+
+    return { save, remove, get, getById, getByCategory }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -29,4 +29,6 @@ module.exports = app => {
         .put(app.api.article.save)
         .delete(app.api.article.remove)
 
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 } 
