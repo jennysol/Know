a062df777c410579dc5b6e571a750486d0fd7873
jennysol <jennyssoliver@gmail.com>
2021-04-13 21:35:42 -0300

    Protecting the api

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,35 +5,43 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
         
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.get)
         .post(app.api.category.save)
 
     // Beware of an order that comes before categories/:id
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
     
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.get)
         .post(app.api.article.save)
     
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
 
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 } 
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const db = require('./config/db')
 app.db = db
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validation.js')
     .then('./api')
