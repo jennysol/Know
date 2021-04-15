08310a34c8f1e656587a87f259cc08e819bf21a6
jennysol <jennyssoliver@gmail.com>
2021-04-15 07:07:45 -0300

    Add template Footer

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="footer"> 
+        <span>Logo <strong>Soliver</strong>TM 2021</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding-right: 10px
+    }
+</style>
