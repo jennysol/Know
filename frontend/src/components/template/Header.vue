7dcfefa6c055301f5b58aa866903d72b1f76db2f
jennysol <jennyssoliver@gmail.com>
2021-04-15 07:03:18 -0300

    Add template Header

---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header"> </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+    }
+</style>
