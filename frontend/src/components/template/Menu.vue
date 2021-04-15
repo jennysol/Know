a5e532713ca325af50c6d187791c004960fd0c6f
jennysol <jennyssoliver@gmail.com>
2021-04-15 07:11:14 -0300

    Add template Menu

---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <aside class="menu"></aside>
+    
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style>
+    .menu {
+        grid-area: menu;
+        background: linear-gradient(to right, #232526, #414345);
+        color: #333;
+
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+</style>
