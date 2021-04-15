e95d5223aceac40b5f0a4f45c65972520af4dfcb
jennysol <jennyssoliver@gmail.com>
2021-04-14 22:04:54 -0300

    Add remove to router

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -15,6 +15,7 @@ module.exports = app => {
         .all(app.config.passport.authenticate())
         .put(admin(app.api.user.save))
         .get(admin(app.api.user.getById))
+        .delete(admin(app.api.user.remove))
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
