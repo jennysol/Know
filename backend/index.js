4f20758ac8bac0f294cb2983e034a17bae521c3f
jennysol <jennyssoliver@gmail.com>
2021-04-14 21:40:48 -0300

    Add Stat to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+const mongoose = require('mongoose')
+require('./config/mongodb')
 
 app.db = db
+app.mongoose = mongoose
 
 consign()
     .include('./config/passport.js')
