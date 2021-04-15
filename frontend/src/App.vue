86457afd4425a876e5d679191c2f10eb6b284d04
jennysol <jennyssoliver@gmail.com>
2021-04-15 07:23:19 -0300

    Add APP initializing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,56 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header/>
+		<Menu/>
+		<Content/>
+		<Footer/>
 	</div>
 </template>
 
 <script>
+import Header from './components/template/Header'
+import Menu from './components/template/Menu'
+import Content from './components/template/Content'
+import Footer from './components/template/Footer'
+
 export default {
 	name: "App",
+	components: {
+		Content,
+		Header,
+		Menu,
+		Footer
+	},
 }
 </script>
 
 <style>
+	* {
+		font-family: 'Mulish', sans-serif;
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
+
+	#app.hide-menu {
+		grid-template-areas:
+			"header header"
+			"content content"
+			"footer footer";
+	}
 </style>
