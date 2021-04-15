70df968d4c95ca8f570b577bdaebea578ed9d16b
jennysol <jennyssoliver@gmail.com>
2021-04-15 06:58:34 -0300

    Add template Content

---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+    
+}
+</script>
+
+<style>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
