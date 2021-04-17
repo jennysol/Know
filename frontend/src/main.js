d24121802cc02a69ff45094315b6fcae748d6a7f
jennysol <jennyssoliver@gmail.com>
2021-04-17 11:05:20 -0300

    Import Bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import App from './App'
 
+import './config/bootstrap'
 import store from './config/store'
 
 Vue.config.productionTip = false
