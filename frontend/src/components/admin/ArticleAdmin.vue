eceb80b4ca02f36042276629c3c81b85a75c3328
jennysol <jennyssoliver@gmail.com>
2021-04-20 21:59:26 -0300

    Adding First component version ArticleAdmin

---
+++ frontend/src/components/admin/ArticleAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="article-admin">
+        <h1>Artigo Componente</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ArticleAdmin',
+
+}
+</script>
+
+<style>
+
+</style>
