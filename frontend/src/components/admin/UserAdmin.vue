63ea369c67368051acb4858b653f8c207ccb2aa1
jennysol <jennyssoliver@gmail.com>
2021-04-20 22:03:47 -0300

    Adding First component version UserAdmin

---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="user-admin">
+        <h1>User Componente</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserAdmin',
+
+}
+</script>
+
+<style>
+
+</style>
