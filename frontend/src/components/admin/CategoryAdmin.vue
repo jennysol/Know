f34b3bde789fb27862f9907549d38ec77a6f5e58
jennysol <jennyssoliver@gmail.com>
2021-04-20 22:02:56 -0300

    Adding First component version CategoryAdmin

---
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="category-admin">
+        <h1>Categorua  Componente</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryAdmin',
+
+}
+</script>
+
+<style>
+
+</style>
