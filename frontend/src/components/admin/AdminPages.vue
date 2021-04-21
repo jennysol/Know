99758f662bb04219c43c8d7b78b641a69d6ee6e2
jennysol <jennyssoliver@gmail.com>
2021-04-20 21:57:48 -0300

    Adding First component version AdminPage

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class="admin-pages">
-        <pageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia"></pageTitle>
+  <div class="admin-pages">
+    <pageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia"></pageTitle>
+    <div class="admin-pages-tabs">
+      <b-cards no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <h1>Artigo</h1>
+          </b-tab>
+          <b-tab title="Categorias" active>
+            <h1>Categorias</h1>
+          </b-tab>
+          <b-tab title="Usuários" active>
+            <h1>Usuário</h1>
+          </b-tab>
+        </b-tabs>
+      </b-cards>
     </div>
+  </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
 
 export default {
-    name: 'AdminPages',
-    components: { PageTitle }
+  name: 'AdminPages',
+  components: { PageTitle }
 
 }
 </script>
