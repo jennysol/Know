e23ade7dfa1e6a2ad2b9cd46a78a4bb27bb9bba7
jennysol <jennyssoliver@gmail.com>
2021-04-20 21:47:46 -0300

    Adding Styles to Stat component

--- frontend/src/components/home/Stat.vue
+++ frontend/src/components/home/Stat.vue
@@ -23,5 +23,39 @@ export default {
 </script>
 
 <style>
+    .stat {
+        flex: 1;
+        display: flex;
+        border-radius: 8px;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        background-color: #FFF;
+        padding: 20px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+    }
 
+    .stat-icon {
+        display: flex;
+        align-items: center;
+    }
+
+    .stat-icon i {
+        font-size: 5rem;
+    }
+
+    .stat-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+
+    .stat-title {
+        font-size: 1.2rem;
+    }
+
+    .stat-value {
+        font-size: 3rem;
+    }
 </style>
