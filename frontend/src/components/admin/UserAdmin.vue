e1abf0053b82bfd707a9d19eb1733479ad94ef81
jennysol <jennyssoliver@gmail.com>
2021-04-22 21:47:56 -0300

    Update: fields in table users

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,13 +1,47 @@
 <template>
-    <div class="user-admin">
-        <h1>User Componente</h1>
-    </div>
+  <div class="user-admin">
+    <b-table hover striped :items="users" :fields="fields"></b-table>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'UserAdmin',
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
 
+export default {
+  name: 'UserAdmin',
+  data: function() {
+    return {
+      mode: 'save',
+      user: {},
+      users: [],
+      fields: [
+        { key: 'id', label: 'Código', sortable: true},
+        { key: 'name', label: 'Nome', sortable: true},
+        { key: 'email', label: 'E-mail', sortable: true},
+        { key: 'admin', 
+          label: 'Administrador', 
+          sortable: true, 
+          formatter: value => value ? 'Sim' : 'Não'
+        },
+        { key: 'actions', label: 'Ações'},
+
+
+      ]
+
+    }
+  },
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`
+      axios.get(url).then(res => {
+        this.users = res.data
+      })
+    }
+  },
+  mounted() {
+    this.loadUsers();
+  }
 }
 </script>
 
