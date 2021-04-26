21ef70c9d22744b0a01f58cd8d5d634c7be528af
jennysol <jennyssoliver@gmail.com>
2021-04-26 07:19:00 -0300

    Update: Form to register User

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="user-admin">
+    <b-form>
+      <input id="user-id" type="hidden" v-model="user.id"/>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Nome:" label-for="user-name">
+            <b-form-input 
+              id="user-name" 
+              type="text" 
+              v-model="user.name" 
+              required 
+              placeholder="Informe o Nome do Usuário">
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="E-mail:" label-for="user-email">
+            <b-form-input 
+              id="user-email" 
+              type="text" 
+              v-model="user.email" 
+              required 
+              placeholder="Informe o Email do Usuário">
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-row>
+
+      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+        Administrador
+      </b-form-checkbox>
+
+       <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Senha:" label-for="user-password">
+            <b-form-input 
+              id="user-password" 
+              type="password" 
+              v-model="user.password" 
+              required 
+              placeholder="Informe a Senha do Usuário">
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="Confirmação de Senha:" label-for="user-confirm-password">
+            <b-form-input 
+              id="user-confirm-password" 
+              type="password" 
+              v-model="user.confirmPassword" 
+              required 
+              placeholder="Confirme a senha do Usuário">
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-form>
     <b-table hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
@@ -25,8 +81,6 @@ export default {
           formatter: value => value ? 'Sim' : 'Não'
         },
         { key: 'actions', label: 'Ações'},
-
-
       ]
 
     }
