b06f7a5c5ca5b5a506dec0367004af405b9ccd60
jennysol <jennyssoliver@gmail.com>
2021-04-29 22:19:33 -0300

    Feature: Articles Item

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -3,7 +3,7 @@
     <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
     <ul>
       <li v-for="article in articles" :key="article.id">
-        {{ article.name }}
+        <ArticleItem :article="article" />
       </li>
     </ul>
     <div class="load-more">
@@ -18,10 +18,11 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
+import ArticleItem from '../article/ArticleItem'
 
 export default {
   name: 'ArticlesByCaregory',
-  components: { PageTitle },
+  components: { PageTitle, ArticleItem },
   data: function() {
     return {
       category: {},
