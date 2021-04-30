4a5d5b73df4bdd486f058c4c925385d6eeb5860f
jennysol <jennyssoliver@gmail.com>
2021-04-29 22:03:31 -0300

    Feature: Articles by Id

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
 import ArticlesByCategory from '@/components/article/ArticlesByCategory'
+import ArticlesById from '@/components/article/ArticlesById'
+
 
 Vue.use(VueRouter)
 
@@ -20,6 +22,10 @@ const routes = [{
     name: 'articleByCaregory',
     path: '/categories/:id/articles',
     component: ArticlesByCategory
+}, {
+    name: 'articleById',
+    path: '/articles/:id',
+    component: ArticlesById
 }]
 
 export default new VueRouter({
